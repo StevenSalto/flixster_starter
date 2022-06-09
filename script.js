741810d8ab94f17044886c335508d63a7c7d1b78
SSalto <73043956+StevenSalto@users.noreply.github.com>
2022-06-09 14:15:51 -0400

    Implemented getMovies and displayMovies functions, site now displays the images of now playing movies

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const API_KEY = "86aa43d0875407669a8288550d29f299";
-
+const moviesOnDisplay = document.querySelector("#movies");
+const POSTER_URL = "https://image.tmdb.org/t/p/w500/";
+//var test_path = "https://api.themoviedb.org/3/configuration?api_key=86aa43d0875407669a8288550d29f299";
+//var testURL = "https://api.themoviedb.org/3/movie/550?api_key=86aa43d0875407669a8288550d29f299"
+var getMoviesURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=86aa43d0875407669a8288550d29f299";
 var query = '';
-document.addEventListener('load', getMovies)
-function getMovies(evt) {
-    evt.
+//document.addEventListener('load', getMovies)
+getMovies();
+async function getMovies() {
+    //console.log("in getMovies function: ")
+    let response = await fetch(getMoviesURL);
+    let responseObj = await response.json();
+    console.log(responseObj);
+    //console.log(await (await fetch(test_path)).json());
+    displayMovies(responseObj);
 }
 
-function displayMovies(){
+function displayMovies(responseObj){
     console.log("displayMovies: ")
+    responseObj.results.forEach((e) => {moviesOnDisplay.innerHTML += `<img src="${POSTER_URL}${e.poster_path}">`});
 }
