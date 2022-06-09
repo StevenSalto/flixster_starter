c5fcf40495f2c31cc6abab7817f1e7cf0150832d
SSalto <73043956+StevenSalto@users.noreply.github.com>
2022-06-09 12:57:56 -0400

    Updated HTML, now displays website name, logo, and a "now playing" section

---
+++ script.js
@@ -0,0 +1,11 @@
+const API_KEY = "86aa43d0875407669a8288550d29f299";
+
+var query = '';
+document.addEventListener('load', getMovies)
+function getMovies(evt) {
+    evt.
+}
+
+function displayMovies(){
+    console.log("displayMovies: ")
+}
